11fcdab104c1413c3bdf1ae89d9583476c69fb78
Bojan Mickoski <bojan.mickoski@armedia.com>
2026-04-30 12:41:54 +0200

    feat: add direct db url cause of websocket based issue

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env.local' });
+config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DIRECT_DATABASE_URL ?? process.env.DATABASE_URL!,
   },
 });
